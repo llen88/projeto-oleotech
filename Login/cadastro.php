140d765321bfc4ebe11f2bc89baa2e7384ed1ec3
Victor Boy <victor2005boy@gmail.com>
2025-06-18 11:02:19 -0300

    .

---
+++ Login/cadastro.php
@@ -0,0 +1,51 @@
+
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+     <link rel="stylesheet" href="Login.css">
+    <title>Criar - OLEOTECH</title>
+</head>
+<body>
+    <div class="login-container">
+
+    <div class="login-illustration">
+        <div class="illustration-content">
+            <h2>OLEOTECH</h2>
+            <p>Descarte consciente, impacto positivo.</p>
+            <div class="abstract-art">
+                <div class="circle circle-blue-light"></div>
+                <div class="circle circle-yellow"></div>
+                <div class="rectangle rectangle-blue-dark"></div>
+                </div>
+            </div>
+        </div>
+    
+        
+
+     <div class="login-form">
+            <div class="logo-area">
+                <!-- <img class="login-logo" src="../img/LOGOTIPO_OLEO_-removebg-preview.png" alt="Logo OLEOTECH"> -->
+                <h2>Criar</h2>
+                <p class="sign-in-text">Você precisa fornecer seus dados de acesso abaixo para entrar no sistema.</p>
+            </div>
+            <form action="#" method="POST">
+                <div class="input-group">
+                    <img class="input-icon" src="../img/profile.png" alt="Ícone de Usuário">
+                    <input type="text" id="username" name="username" placeholder="Nome de Usuário">
+                </div>
+                <div class="input-group">
+                    <img class="input-icon lock-icon" src="../img/padlock.png" alt="Ícone de Cadeado">
+                    <input type="password" id="password" name="password" placeholder="Senha">
+                </div>
+                <a href="Main Page/index.html" target="_self"><button type="submit" class="login-button">Entrar</button></a>
+            </form>
+            <div class="links-area">
+                <a href="#">Esqueceu sua senha?</a>
+            </div>
+            <p class="copyright">© OLEOTECH</p>
+        </div>
+    </div>
+    
+</body>
+</html>
